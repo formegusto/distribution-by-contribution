8c15757d97d585eee2b6bd8a1b93e3be7a4a2701
formegusto <formegusto@gmail.com>
2022-03-02 10:25:00 +0900

    initial commit

---
+++ src/crs/models/__init__.py
@@ -0,0 +1,7 @@
+from models.Household import Household
+from models.Public import Public
+from models.ManagementOffice import ManagementOffice
+
+
+__all__ = ['Household', 'Public', 'ManagementOffice']
+__version__ = '0.1.0'
