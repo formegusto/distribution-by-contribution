085078e10875f1e87b10e3a1a0467b45526e4f48
formegusto <formegusto@gmail.com>
2022-03-04 11:19:21 +0900

    update : time division kmeans set K (rule of thumbs)

--- src/dbc/TimeDivisionKMeans.py
+++ src/dbc/TimeDivisionKMeans.py
@@ -1,3 +1,18 @@
+import math as mt
+
+
 class TimeDivisionKMeans:
-    def __init__(self):
-        pass
+    def __init__(self, df):
+        self.df = df
+        self.setK()
+
+    def setK(self):
+        households_cnt = len(self.df.columns)
+
+        print("-----[setK info]-----")
+
+        print("data length : {}".format(households_cnt))
+        self.K = mt.ceil(mt.sqrt(households_cnt / 2))
+        print("K is {}".format(self.K))
+
+        print("---------------------")
