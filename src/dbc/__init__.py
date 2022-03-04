67c7160d02d4abc26d5ccbf4a8f2b20affabc4c5
formegusto <formegusto@gmail.com>
2022-03-04 10:32:54 +0900

    update : data preprocessing 간소화

--- src/dbc/__init__.py
+++ src/dbc/__init__.py
@@ -1,4 +1,4 @@
-from dbc.TimeDivisionKMeans import TimeDivisionKMeans
+from src.dbc.TimeDivisionKMeans import TimeDivisionKMeans
 
 __all__ = ["TimeDivisionKMeans"]
 __version__ = "0.1.0"
--- src/dbc/utils/__init__.py
+++ src/dbc/utils/__init__.py
@@ -1,4 +1,4 @@
-from dbc.utils import data_preprocessing
+from src.dbc.utils.data_preprocessing import data_preprocessing
 
 __all__ = ["data_preprocessing"]
 __version__ = "0.1.0"
