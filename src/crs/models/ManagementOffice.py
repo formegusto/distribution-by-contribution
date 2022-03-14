303858376b1bb146e691a9963db14f998682d256
formegusto <formegusto@gmail.com>
2022-03-14 15:04:14 +0900

    update : 기여도 산정 공동설비사용요금

--- src/crs/models/ManagementOffice.py
+++ src/crs/models/ManagementOffice.py
@@ -5,8 +5,9 @@ import math as mt
 
 class ManagementOffice:
     def __init__(self,
-                 month, peaks, households, APT,  # datas
+                 month, households, APT,  # datas
                  contract,
+                 peaks=None,
                  general_fee_info=None):
         self.peaks = peaks
         self.APT = APT
@@ -14,95 +15,51 @@ class ManagementOffice:
         self.contract = contract
         self.select_fee(month, contract, general_fee_info)
 
-        if contract == "종합계약":
-            # 가구 객체화
-            # - 가구별 지정된 계약에 요금들이 계산되도록 Property 구성해놨음.
-            self.households = list()
-            for idx in households.index:
-                self.households.append(
-                    Household(
-                        name=households.iloc[idx]['name'],
-                        kwh=households.iloc[idx]['usage (kWh)'],
-                        contract=self.fee[0],
-                        contract_name=contract
-                    )
+        num_household = len(households)
+        mean_kwh = round(APT / num_household)
+
+        self.apart = Household(
+            name="아파트",
+            kwh=mean_kwh,
+            contract=self.fee,
+            contract_name=contract
+        )
+
+        basic = self.apart.basic * num_household
+        elec_rate = self.apart.elec_rate * num_household
+        env = self.apart.env * num_household
+        fuel = self.apart.fuel * num_household
+
+        elec_bill = basic + elec_rate + env - fuel
+        self.bill = mt.floor((elec_bill
+                              + round(elec_bill * 0.1)
+                              + mt.floor(elec_bill * 0.037 * 0.1) * 10) * 0.1) * 10
+
+        # 가구 객체화
+        # - 가구별 지정된 계약에 요금들이 계산되도록 Property 구성해놨음.
+        self.households = list()
+        for idx in households.index:
+            self.households.append(
+                Household(
+                    name=households.iloc[idx]['name'],
+                    kwh=households.iloc[idx]['usage (kWh)'],
+                    contract=self.fee,
+                    contract_name=contract
                 )
-
-            # 공용설비사용요금
-            households_kwh = sum([_.kwh for _ in self.households])
-            public_kwh = APT - households_kwh
-            max_peak = peaks['peak (kW)'].max()
-            charge_applied = max_peak * (public_kwh / APT)
-
-            self.public = Public(
-                kwh=public_kwh,
-                charge_applied=charge_applied,
-                contract=self.fee[1]
-            )
-            self.public_bill = self.public.elec_bill_vat_fund
-
-            # 가구별 청구서 셋팅
-            public_fee = round(self.public_bill /
-                               len(self.households) / 10) * 10
-
-            for household in self.households:
-                household.set_bill(
-                    public_fee=public_fee
-                )
-
-            # 아파트 청구서 셋팅
-            bill = 0
-            for household in self.households:
-                bill += household.bill
-            self.bill = bill
-        elif contract == "단일계약":
-            # 전체를 하나의 가구 본 상태에서 계약대로 계산식 진행
-            # 후에 * len(household) 를 통해 아파트 전체 요금 통지서 확보
-            num_household = len(households)
-            mean_kwh = round(APT / num_household)
-
-            self.apart = Household(
-                name="아파트",
-                kwh=mean_kwh,
-                contract=self.fee,
-                contract_name=contract
             )
 
-            basic = self.apart.basic * num_household
-            elec_rate = self.apart.elec_rate * num_household
-            env = self.apart.env * num_household
-            fuel = self.apart.fuel * num_household
+        # 공공설비사용요금
+        households_bill = sum(
+            [_.elec_bill_vat_fund for _ in self.households]
+        )
+        self.public_bill = self.bill - households_bill
 
-            elec_bill = basic + elec_rate + env - fuel
-            self.bill = mt.floor((elec_bill
-                                  + round(elec_bill * 0.1)
-                                  + mt.floor(elec_bill * 0.037 * 0.1) * 10) * 0.1) * 10
-
-            # 가구 객체화
-            # - 가구별 지정된 계약에 요금들이 계산되도록 Property 구성해놨음.
-            self.households = list()
-            for idx in households.index:
-                self.households.append(
-                    Household(
-                        name=households.iloc[idx]['name'],
-                        kwh=households.iloc[idx]['usage (kWh)'],
-                        contract=self.fee,
-                        contract_name=contract
-                    )
-                )
-
-            # 공공설비사용요금
-            households_bill = sum(
-                [_.elec_bill_vat_fund for _ in self.households]
+        # 가구별 청구서 셋팅
+        public_fee = round(self.public_bill / len(self.households))
+        for household in self.households:
+            household.set_bill(
+                public_fee=public_fee
             )
-            self.public_bill = self.bill - households_bill
-
-            # 가구별 청구서 셋팅
-            public_fee = round(self.public_bill / len(self.households))
-            for household in self.households:
-                household.set_bill(
-                    public_fee=public_fee
-                )
 
     # 요금제 셋팅 메서드
     def select_fee(self, month, contract, general_fee_info):
